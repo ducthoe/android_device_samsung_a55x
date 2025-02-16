11958bd7742c3f19b093ca7ff0728d046f3015e3
Secondarian <robertanitei@tutanota.com>
2025-02-17 00:17:26 +0200

    e1s: Initial camera setup

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,9 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('system', 'secure_element'),
     'vendor/etc/init/init.s5e9945.rc': blob_fixup()
         .regex_replace('vendor_spay', 'system'),
+    'vendor/etc/init/vendor.samsung.hardware.camera.provider-service_64.rc': blob_fixup()
+        .regex_replace('vendor_secdir w', 'w')
+        .regex_replace('vendor_secdir', 'camera'),
     (
         'vendor/lib64/hw/audio.primary.s5e9945.so',
         'vendor/lib64/libaudioproxy2.so',
@@ -37,6 +40,10 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute_samsung.so')
         .replace_needed('libtinyalsa.so', 'libtinyalsa_samsung.so'),
+    'vendor/lib64/hw/camera.s5e9945.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    'vendor/lib64/lib_profiler.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full-21.7.so', 'libprotobuf-cpp-full-21.12.so'),
     (
         'vendor/lib64/libalsautils_sec.so',
         'vendor/lib64/libaudioroute_samsung.so',
@@ -49,6 +56,11 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace(
             '0e 40 f9 e1 03 16 aa 82 0c 80 52 e3 03 15 aa',
             '0e 40 f9 e1 03 16 aa 82 0c 80 52 03 00 80 d2'),
+    (
+        'vendor/lib64/libsensorlistener.so',
+        'vendor/lib64/libvdis_core.so',
+    ): blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so'),
     'vendor/lib64/libskeymint_cli.so': blob_fixup()
         .add_needed('libshim_crypto.so'),
 }  # fmt: skip
