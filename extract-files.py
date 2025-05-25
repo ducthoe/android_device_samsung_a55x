6d5a1896fa33b1e0ffbbc78219087f0d2638d751
borbelyvince <borbely.vince.11@gmail.com>
2025-05-25 11:19:55 +0200

    e2s: Decommonize non-universal blobs

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,9 +18,19 @@ namespace_imports = [
     'vendor/samsung/s5e9945',
 ]
 
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/hw/audio.primary.s5e9945.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute_samsung.so')
+        .replace_needed('libtinyalsa.so', 'libtinyalsa_samsung.so'),
+    'vendor/lib64/hw/camera.s5e9945.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+}
+
 module = ExtractUtilsModule(
     'e2s',
     'samsung',
+    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
 )
 
